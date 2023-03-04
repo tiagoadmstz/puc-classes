58b6d8d9c1f66357fa0c7b1b089dba9d9ecba824
Tiago Daniel Teixeira <tiagoadmstz@yahoo.com.br>
2023-03-03 23:36:42 -0300

    added api to get movies list

--- curso6/my-movies/src/App.js
+++ curso6/my-movies/src/App.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
+import { MoviesService } from "./api/MoviesService";
 import { Header } from "./components/Header";
 import { MoviesList } from "./components/MoviesList";
 import { MovieContainer } from "./styles/MoviesContainer";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = async () => {
+    const { data } = await MoviesService.getMovies();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <Header />
 
       <MovieContainer>
-        <MoviesList movies={[{title:'teste'}]}/>
+        <MoviesList movies={ movies }/>
       </MovieContainer>
     </div>
   );
