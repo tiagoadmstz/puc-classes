6785afbe5d9991f736514eaf18363eced00fde98
Tiago Daniel Teixeira <tiagoadmstz@yahoo.com.br>
2023-03-06 21:38:25 -0300

    create my-movies-modular project

---
+++ curso6/my-movies-modular/env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$(ls -A /home/root/my-movies-modular/src/)" ]; then
+  npx create-react-app my-movies-modular
+  mv -f my-movies-modular/* ../my-movies-modular/
+  mv -f ./my-movies-modular/.gitignore ../my-movies-modular/
+  rm -R my-movies-modular
+  chmod -R 777 .
+  npm i react-router-dom styled-components axios yarn nodemon
+fi
+
+npm i
+yarn start
+
+exit 0
--- curso6/my-movies/env.sh
+++ curso6/my-movies/env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ -z "$(ls -A /home/root/my-movies/)" ]; then
-  npx create-react-app ../my-movies
+  npx create-react-app my-movies
+  mv -f my-movies/* ../my-movies/
+  mv -f ./my-movies/.gitignore ../my-movies/
+  rm -R my-movies
+  chmod -R 777 .
   npm i react-router-dom styled-components axios yarn nodemon
 fi
 
