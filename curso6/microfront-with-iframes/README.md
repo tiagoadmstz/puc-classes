3ba44738cbab3c67f916b52864d8255726e0cadb
Tiago Daniel Teixeira <tiagoadmstz@yahoo.com.br>
2023-03-09 09:45:36 -0300

    added example projects

---
+++ curso6/microfront-with-iframes/README.md
@@ -0,0 +1,7 @@
+# Instruções
+
+* Entre na pasta `satellite` via linha de comando e rode o comando `npm install && npm start`.
+
+* Em seguida, faça o mesmo dentro das pastas `fragment-1` e `fragment-2`.
+
+* Entre em `http://localhost:8000` e veja a aplicação rodando.
---
+++ curso6/microfront-with-iframes/satellite/eventReceivers.js
@@ -0,0 +1,30 @@
+import { getUsers } from './services/service1';
+
+const FRAGMENT_EVENT_PREFIX = 'fragmentEvent:';
+const shouldHandleMessage = (msg) =>
+    Object.keys(msg).find(
+        (k) => k == 'action' && msg.action.startsWith(FRAGMENT_EVENT_PREFIX)
+    );
+
+export const handleEvent = (fragmentEvent) => {
+    if (
+        !fragmentEvent.data ||
+        !fragmentEvent.data.message ||
+        !shouldHandleMessage(fragmentEvent.data.message)
+    ) {
+        return;
+    }
+
+    const { message, trackingProperties } = fragmentEvent.data;
+    const payload = {
+        message,
+        trackingProperties,
+        source: fragmentEvent.source,
+    };
+
+    switch (message.action) {
+        case `${FRAGMENT_EVENT_PREFIX}getUser`:
+            getUsers(payload);
+            break;
+    }
+};
